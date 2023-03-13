aa8c0262cc3011e5a47c0977699e5c41bdc86d69
Ashwanth <ashwanthbalakrishnan5@gmail.com>
2023-03-13 17:27:24 +0530

    added home page to website

--- BitTalk/urls.py
+++ BitTalk/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('binance/', include('Binance.urls'))
+    path('binance/', include('Binance.urls')),
+    path('', views.Home)
 ]
